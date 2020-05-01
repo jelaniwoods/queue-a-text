2e0657ea31004db76e545656b0036dcea559fb7d
Jelani Woods <jelani@firstdraft.com>
2020-04-30 21:26:52 -0500

    Clean up Message routes and remove extra nav links.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   root "messages#index"
-  resources :messages
+  resources :messages, except: :show
   resources :contacts
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
